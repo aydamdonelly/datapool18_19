6c45cbba66953dd982c6e2aa5f8efc8183b5c28f
Adrian Selk <adrian.selk@ibm.com>
2024-05-20 09:11:51 +0200

    cleaned code

--- adam/visualizations/modules/cl_results.py
+++ adam/visualizations/modules/cl_results.py
@@ -6,38 +6,51 @@ from dash import dcc, html
 df_results = pd.read_csv("../../adrian/data/cl_results.csv")
 df_players = pd.read_csv("../../adrian/data/cl_season_standard_stats.csv")
 
-# calculate metrics
+# calculate total goals scored, conceded, wins, draws, and losses (cols 1 and 2)
 total_goals_scored = df_results['GF'].sum()
 total_goals_conceded = df_results['GA'].sum()
 total_wins = (df_results['Result'] == 'W').sum()
 total_draws = (df_results['Result'] == 'D').sum()
 total_losses = (df_results['Result'] == 'L').sum()
 
+# calculate total goals and assists by players (cols 3 and 4)
 player_goals = df_players.groupby('Player')['Gls'].sum().sort_values(ascending=False)
 player_assists = df_players.groupby('Player')['Ast'].sum().sort_values(ascending=False)
 
+# filter for players with at least one goal or assist
 players_with_goals = set(player_goals[player_goals > 0].index)
 players_with_assists = set(player_assists[player_assists > 0].index)
 players_with_goals_or_assists = players_with_goals.union(players_with_assists)
 
+# define function to generate random unqiue colors 
+# every player should have a distinct color
 def generate_color_gradient(index, max_index):
     red = (255, 0, 0)
     blue = (0, 0, 255)
+    
+    # calculate proportions of red and blue based on the player's index
     red_proportion = 1 - ((index / max_index) ** 0.5)  
     blue_proportion = (index / max_index) ** 0.5 
+    
+    # calculate RGB values for the color based on the gradient
     r = int(red[0] * red_proportion + blue[0] * blue_proportion)
     g = int(red[1] * red_proportion + blue[1] * blue_proportion)
     b = int(red[2] * red_proportion + blue[2] * blue_proportion)
+    
+    # format rgb into hex code
     return "#{:02x}{:02x}{:02x}".format(r, g, b)
 
+# create mapping of players to colors based on the color gradient
 player_colors = {player: generate_color_gradient(index, len(players_with_goals_or_assists) - 1) 
                  for index, player in enumerate(players_with_goals_or_assists)}
 
+# calculate total number of "bricks" (one rectangle) for each column
 total_bricks_goals = total_goals_scored + total_goals_conceded
 total_bricks_results = total_wins + total_draws + total_losses
 total_bricks_players_goals = player_goals.sum()
 total_bricks_players_assists = player_assists.sum()
 
+# calculate brick height for each column (all columns should have same size in the end)
 brick_height_goals = 100 / total_bricks_goals
 brick_height_results = 100 / total_bricks_results
 brick_height_players_goals = 100 / total_bricks_players_goals
@@ -50,6 +63,7 @@ def create_figure():
     column_width = 0.5
     column_positions = [0, 1.0, 2.0, 3.0]
 
+# trace for goals scored (col 1)
     for i in range(total_goals_scored):
         fig.add_shape(
             type="rect",
@@ -70,7 +84,7 @@ def create_figure():
             hoverinfo="text",
             text=f"Total Goals Scored: {total_goals_scored}"
         ))
-    
+    # trace for goals conceded (col 1)
     for i in range(total_goals_conceded):
         fig.add_shape(
             type="rect",
@@ -91,7 +105,7 @@ def create_figure():
             hoverinfo="text",
             text=f"Total Goals Conceded: {total_goals_conceded}"
         ))
-        
+    # trace for wins (col 2)
     for i in range(total_wins):
             fig.add_shape(
                 type="rect",
@@ -112,7 +126,7 @@ def create_figure():
                 hoverinfo="text",
                 text=f"Win {i + 1}"
             ))
-            
+      # trace for draws (col 2)      
     for i in range(total_draws):
             fig.add_shape(
                 type="rect",
@@ -134,6 +148,7 @@ def create_figure():
                 text=f"Draw {i + 1}"
             ))
 
+# trace for losses (col 2)
     for i in range(total_losses):
         fig.add_shape(
             type="rect",
@@ -155,7 +170,7 @@ def create_figure():
             text=f"Loss {i + 1}"
         ))
 
-            
+    # scatter trace for goals by players (col 3)
     current_height_goals = 0
     for player, goals in player_goals.items():
         cumulative_goals = df_players[df_players['Player'] == player]['Gls'].sum()
@@ -180,7 +195,7 @@ def create_figure():
                 text=f"{player}: Total Goals Scored: {cumulative_goals}"
             ))
         current_height_goals += goals * brick_height_players_goals
-
+# scatter trace for assists by players (col 4)
     current_height_assists = 0
     for player, assists in player_assists.items():
         cumulative_assists = df_players[df_players['Player'] == player]['Ast'].sum()
